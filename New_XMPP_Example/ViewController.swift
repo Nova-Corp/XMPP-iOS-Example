ef3d6c0a0d5fdf3a25ac7709dd891b0a6a415366
Shanmugarajeshwaran R <tweets.shan@gmail.com>
2020-02-25 20:00:42 +0530

    Initial Commit

---
+++ New_XMPP_Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  New_XMPP_Example
+//
+//  Created by ADMIN on 25/02/20.
+//  Copyright © 2020 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
